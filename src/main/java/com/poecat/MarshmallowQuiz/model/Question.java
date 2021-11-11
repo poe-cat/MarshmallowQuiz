762dc5033214969d61fc553eaf8af62d40863a1f
Kasia <katarzyna.tramea@gmail.com>
2021-11-11 19:27:06 +0100

    add data fields & empty constructor in Question.java

--- src/main/java/com/poecat/MarshmallowQuiz/model/Question.java
+++ src/main/java/com/poecat/MarshmallowQuiz/model/Question.java
@@ -1,4 +1,28 @@
 package com.poecat.MarshmallowQuiz.model;
 
+import org.springframework.stereotype.Component;
+import javax.persistence.*;
+
+@Component
+@Entity
+@Table(name = "questions")
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int quesId;
+    private String title;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private int ans;
+    private int chose;
+
+    public Question() {
+        //empty?
+    }
+
+
+
+
 }
