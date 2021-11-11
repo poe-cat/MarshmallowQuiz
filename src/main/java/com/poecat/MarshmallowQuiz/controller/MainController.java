6de3c0780b3e84c127936ed201b4d42e0fc87a09
Kasia <katarzyna.tramea@gmail.com>
2021-11-11 19:31:18 +0100

    add fields in MainController and QuizService

--- src/main/java/com/poecat/MarshmallowQuiz/controller/MainController.java
+++ src/main/java/com/poecat/MarshmallowQuiz/controller/MainController.java
@@ -1,7 +1,19 @@
 package com.poecat.MarshmallowQuiz.controller;
 
+import com.poecat.MarshmallowQuiz.model.Result;
+import com.poecat.MarshmallowQuiz.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    Result result;
+    @Autowired
+    QuizService qService;
+
+
+
+
 }
---
+++ src/main/java/com/poecat/MarshmallowQuiz/service/QuizService.java
@@ -0,0 +1,7 @@
+package com.poecat.MarshmallowQuiz.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+}
