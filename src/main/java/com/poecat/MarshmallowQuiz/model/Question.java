c8cb11f51aec9223e7e7d0b61c369c34b006f5cd
Kasia <katarzyna.tramea@gmail.com>
2021-11-11 19:43:06 +0100

    create constructor, toString(), getters & setters in Question.java

--- src/main/java/com/poecat/MarshmallowQuiz/model/Question.java
+++ src/main/java/com/poecat/MarshmallowQuiz/model/Question.java
@@ -22,7 +22,82 @@ public class Question {
         //empty?
     }
 
+    public Question(int quesId, String title, String optionA,
+                    String optionB, String optionC, int answer, int choice) {
+        super();
+        this.quesId = quesId;
+        this.title = title;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.answer = answer;
+        this.choice = choice;
+    }
 
+    public int getQuesId() {
+        return quesId;
+    }
 
+    public void setQuesId(int quesId) {
+        this.quesId = quesId;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public int getChoice() {
+        return choice;
+    }
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+    @Override
+    public String toString() {
+        return "Question [quesId=" + quesId + ", " +
+                "title=" + title + ", " +
+                "optionA=" + optionA + ", " +
+                "optionB=" + optionB + ", " +
+                "optionC=" + optionC + ", " +
+                "answer=" + answer + ", " +
+                "choice=" + choice + "]";
+    }
 }
