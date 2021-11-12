16be2171e6e44223a591f4f42a09a5e30e8cc6b3
Kasia <katarzyna.tramea@gmail.com>
2021-11-12 17:36:12 +0100

    created quiz() & mapping

--- src/main/java/com/poecat/MarshmallowQuiz/controller/MainController.java
+++ src/main/java/com/poecat/MarshmallowQuiz/controller/MainController.java
@@ -1,9 +1,16 @@
 package com.poecat.MarshmallowQuiz.controller;
 
+import com.poecat.MarshmallowQuiz.model.QuestionForm;
 import com.poecat.MarshmallowQuiz.model.Result;
 import com.poecat.MarshmallowQuiz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -12,6 +19,33 @@ public class MainController {
     Result result;
     @Autowired
     QuizService qService;
+
+    Boolean submitted = false;
+
+    @ModelAttribute("result")
+    public Result getResult() {
+        return result;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
+
+    @PostMapping("/quiz")
+    public String quiz(@RequestParam String username, Model m, RedirectAttributes ra) {
+        if(username.equals("")) {
+            ra.addFlashAttribute("Hold on...", "Please enter your name");
+            return "redirect:/";
+        }
+        submitted = false;
+        result.setUsername(username);
+
+        QuestionForm qForm = qService.getQuestions();
+        m.addAttribute("qForm", qForm);
+
+        return "quiz.html";
+    }
 
 
 
