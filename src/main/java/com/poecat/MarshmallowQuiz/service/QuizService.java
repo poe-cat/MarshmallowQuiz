1a383933052d27a7ffcc05b06579b612c73e44c9
Kasia <katarzyna.tramea@gmail.com>
2021-11-12 17:00:39 +0100

    update QuizService with getQuestions() (creates questions list from repo)

--- src/main/java/com/poecat/MarshmallowQuiz/service/QuizService.java
+++ src/main/java/com/poecat/MarshmallowQuiz/service/QuizService.java
@@ -1,7 +1,44 @@
 package com.poecat.MarshmallowQuiz.service;
 
+import com.poecat.MarshmallowQuiz.model.Question;
+import com.poecat.MarshmallowQuiz.model.QuestionForm;
+import com.poecat.MarshmallowQuiz.model.Result;
+import com.poecat.MarshmallowQuiz.repo.QuestionRepo;
+import com.poecat.MarshmallowQuiz.repo.ResultRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class QuizService {
+
+    @Autowired
+    Question question;
+    @Autowired
+    QuestionForm qForm;
+    @Autowired
+    QuestionRepo qRepo;
+
+    @Autowired
+    Result result;
+    @Autowired
+    ResultRepo rRepo;
+
+    public QuestionForm getQuestions() {
+        List<Question> allQues = qRepo.findAll();
+        List<Question> qList = new ArrayList<Question>();
+
+        Random random = new Random();
+
+        for(int i=0; i<5; i++) {
+            int rand = random.nextInt(allQues.size());
+            qList.add(allQues.get(rand));
+            allQues.remove(rand);
+        }
+        qForm.setQuestions(qList);
+
+        return qForm;
+    }
 }
