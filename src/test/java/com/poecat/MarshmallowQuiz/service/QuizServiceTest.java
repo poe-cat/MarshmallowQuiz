f3c0bf4a9410b2816693eb4177089add39ccb151
Kasia <katarzyna.tramea@gmail.com>
2021-11-12 17:07:01 +0100

    create test class QuizServiceTest

---
+++ src/test/java/com/poecat/MarshmallowQuiz/service/QuizServiceTest.java
@@ -0,0 +1,12 @@
+package com.poecat.MarshmallowQuiz.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizServiceTest {
+
+    @Test
+    void getQuestions() {
+    }
+}
