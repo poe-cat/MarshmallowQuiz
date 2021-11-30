a26105add4074c837bf1bde1e37f95d69ca6f71f
Kasia <katarzyna.tramea@gmail.com>
2021-11-30 15:11:46 +0100

    removed unused annotations (problem with non-existing repositories solved -> just moved main class out of service folder...)

--- src/main/java/com/poecat/quiz/MarshmallowQuizApplication.java
+++ src/main/java/com/poecat/quiz/MarshmallowQuizApplication.java
@@ -1,10 +1,8 @@
-package com.poecat.MarshmallowQuiz.service;
+package com.poecat.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.poecat.MarshmallowQuiz.repo")
 @SpringBootApplication
 public class MarshmallowQuizApplication {
 
