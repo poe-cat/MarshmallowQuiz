372c629b8aa87b7a88d1fc8c703559e6fe2d6a5f
Kasia <katarzyna.tramea@gmail.com>
2021-12-07 12:47:47 +0100

    changed "role" to "authority" in Http security (SecurityConfig)

--- src/main/java/com/poecat/quiz/security/SecurityConfig.java
+++ src/main/java/com/poecat/quiz/security/SecurityConfig.java
@@ -27,8 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER_ROLE","ADMIN_ROLE")
+                .antMatchers("/admin").hasAuthority("ADMIN")
+                .antMatchers("/user").hasAnyAuthority("USER","ADMIN")
                 .antMatchers("/").permitAll()
                 .and().formLogin()
                 .and().csrf().disable();
