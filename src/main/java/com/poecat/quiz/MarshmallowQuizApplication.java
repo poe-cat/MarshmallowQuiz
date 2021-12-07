75f6af61275f81e28619f54f92fc07111e70f35b
Kasia <katarzyna.tramea@gmail.com>
2021-12-07 12:45:30 +0100

    enabled jpa in main class

--- src/main/java/com/poecat/quiz/MarshmallowQuizApplication.java
+++ src/main/java/com/poecat/quiz/MarshmallowQuizApplication.java
@@ -1,9 +1,12 @@
 package com.poecat.quiz;
 
+import com.poecat.quiz.repo.UserRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepo.class)
 public class MarshmallowQuizApplication {
 
 	public static void main(String[] args) {
