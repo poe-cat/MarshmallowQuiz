57d334614390fa822c92ce885a73d4583c72db79
Kasia <katarzyna.tramea@gmail.com>
2021-12-07 12:01:02 +0100

    updated MyUserDetails with its service with database mapping

--- src/main/java/com/poecat/quiz/service/MyUserDetailsService.java
+++ src/main/java/com/poecat/quiz/service/MyUserDetailsService.java
@@ -1,16 +1,25 @@
 package com.poecat.quiz.service;
 
 import com.poecat.quiz.MyUserDetails;
+import com.poecat.quiz.model.User;
+import com.poecat.quiz.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepo userRepo;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepo.findByUsername(username);
+        return new MyUserDetails(user);
     }
 }
