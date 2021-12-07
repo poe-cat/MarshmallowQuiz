8db3766503870208842eb2a6de41c1a2a86f72ec
Kasia <katarzyna.tramea@gmail.com>
2021-12-07 12:49:09 +0100

    updated UserNotFoundException message

--- src/main/java/com/poecat/quiz/service/MyUserDetailsService.java
+++ src/main/java/com/poecat/quiz/service/MyUserDetailsService.java
@@ -1,6 +1,6 @@
 package com.poecat.quiz.service;
 
-import com.poecat.quiz.MyUserDetails;
+import com.poecat.quiz.model.MyUserDetails;
 import com.poecat.quiz.model.User;
 import com.poecat.quiz.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +20,9 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepo.findByUsername(username);
-        return new MyUserDetails(user);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+
+        return user.map(MyUserDetails::new).get();
     }
 }
