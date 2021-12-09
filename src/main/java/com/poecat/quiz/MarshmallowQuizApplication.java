e351fc5567ad2f83cc536f2a5e60c09b80c987e7
Kasia <katarzyna.tramea@gmail.com>
2021-12-09 19:05:24 +0100

    added sendEmail() to main class

--- src/main/java/com/poecat/quiz/MarshmallowQuizApplication.java
+++ src/main/java/com/poecat/quiz/MarshmallowQuizApplication.java
@@ -1,16 +1,30 @@
 package com.poecat.quiz;
 
 import com.poecat.quiz.repo.UserRepo;
+import com.poecat.quiz.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepo.class)
 public class MarshmallowQuizApplication {
 
+	@Autowired
+	private EmailSenderService senderService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarshmallowQuizApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail() {
+		senderService.sendEmail("someone@mail.com",
+				"First to ecounter",
+				"This is my first mail to you, dear.");
 	}
 
 }
