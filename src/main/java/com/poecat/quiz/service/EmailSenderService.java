fd2796ee4263e3bdab938b6a41537c72bcce622f
Kasia <katarzyna.tramea@gmail.com>
2021-12-11 15:03:50 +0100

    commented EmailSenderService.java (for now)

--- src/main/java/com/poecat/quiz/service/EmailSenderService.java
+++ src/main/java/com/poecat/quiz/service/EmailSenderService.java
@@ -7,21 +7,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService {
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendEmail(String toEmail,
-                          String subject,
-                          String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("yourMail@mail.com");
-        message.setTo(toEmail);
-        message.setText(body);
-        message.setSubject(subject);
-
-        mailSender.send(message);
-
-        System.out.println("Mail sent successfully!");
-    }
+//
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void sendEmail(String toEmail,
+//                          String subject,
+//                          String body) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom("yourMail@mail.com");
+//        message.setTo(toEmail);
+//        message.setText(body);
+//        message.setSubject(subject);
+//
+//        mailSender.send(message);
+//
+//        System.out.println("Mail sent successfully!");
+//    }
 }
