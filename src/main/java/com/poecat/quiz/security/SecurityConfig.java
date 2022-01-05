201753db49a2aaf71a5a11d9e04234a92cb36f51
Kasia <katarzyna.tramea@gmail.com>
2022-01-05 19:53:06 +0100

    add successHandler() to authentication for logging in SecurityConfig

--- src/main/java/com/poecat/quiz/security/SecurityConfig.java
+++ src/main/java/com/poecat/quiz/security/SecurityConfig.java
@@ -7,9 +7,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -31,8 +38,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user").hasAnyAuthority("USER","ADMIN")
                 .antMatchers("/").permitAll()
                 .and().formLogin()
-                .loginPage("/login.html")
+                .defaultSuccessUrl("/indexU")
+                .failureUrl("/login_error")
+                .successForwardUrl("/login_success_handler")
+                .failureForwardUrl("/login_failure_handler")
+                .loginPage("/login")
+                .usernameParameter("username")
+                .permitAll()
+                .and()
+                .logout().permitAll()
+                .and().rememberMe()
                 .and().csrf().disable();
+
+        http.formLogin().successHandler(new AuthenticationSuccessHandler() {
+
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                Authentication authentication) throws IOException, ServletException {
+
+                System.out.println("Logged user: " + authentication.getName());
+
+                response.sendRedirect("/user");
+            }
+        });
 
     }
 
