606f64da69e90967b1b3addb324193a15eeee74c
Kasia <katarzyna.tramea@gmail.com>
2022-01-06 10:24:22 +0100

    add customized logout to SecurityConfig

--- src/main/java/com/poecat/quiz/security/SecurityConfig.java
+++ src/main/java/com/poecat/quiz/security/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout().permitAll()
+                .logoutUrl("/doLogout")
                 .and().rememberMe()
                 .and().csrf().disable();
 
