319f24d103951663a5644cc906ea84026a23593d
Kasia <katarzyna.tramea@gmail.com>
2022-01-11 12:43:06 +0100

    add failureHandler for login form in SecurityConfig

--- src/main/java/com/poecat/quiz/security/SecurityConfig.java
+++ src/main/java/com/poecat/quiz/security/SecurityConfig.java
@@ -3,14 +3,17 @@ package com.poecat.quiz.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
@@ -60,6 +63,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 System.out.println("Logged user: " + authentication.getName());
 
                 response.sendRedirect("/user");
+            }
+        });
+
+        http.formLogin().failureHandler(new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                                AuthenticationException e) throws IOException, ServletException {
+
+                System.out.println("Cannot log in as user: " + e.getMessage());
+
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.sendRedirect("/login?loginError=true");
             }
         });
 
