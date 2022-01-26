13fb9d76591f8adf7fb3490f4f37bc56e6ab806c
Kasia <katarzyna.tramea@gmail.com>
2022-01-26 19:37:44 +0100

    delete unused import

--- src/main/java/com/poecat/quiz/model/MyUserDetails.java
+++ src/main/java/com/poecat/quiz/model/MyUserDetails.java
@@ -1,6 +1,5 @@
 package com.poecat.quiz.model;
 
-import com.poecat.quiz.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
