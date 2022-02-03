2c94afcc8ae95b243f5676683a37de92665466c4
Kasia <katarzyna.tramea@gmail.com>
2022-02-03 16:36:11 +0100

    delete Optional from UserRepo, instead add query. CrudRepository extension instead of JpaRepository

--- src/main/java/com/poecat/quiz/repo/UserRepo.java
+++ src/main/java/com/poecat/quiz/repo/UserRepo.java
@@ -1,11 +1,12 @@
 package com.poecat.quiz.repo;
 
 import com.poecat.quiz.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+public interface UserRepo extends CrudRepository<User,Integer> {
 
-public interface UserRepo extends JpaRepository<User,Integer> {
-
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
 }
