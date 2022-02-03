94b3bb06e054eccb10326ccfc9cd9c9d94fedcaf
Kasia <katarzyna.tramea@gmail.com>
2022-02-03 16:32:16 +0100

    update user roles list in MyUserDetails.java

--- src/main/java/com/poecat/quiz/model/MyUserDetails.java
+++ src/main/java/com/poecat/quiz/model/MyUserDetails.java
@@ -3,41 +3,37 @@ package com.poecat.quiz.model;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
+import java.util.*;
 
 public class MyUserDetails implements UserDetails {
 
-    private String username;
-    private String password;
-    private boolean active;
-    private List<GrantedAuthority> authorities;
+    private User user;
 
     public MyUserDetails(User user) {
-        this.username = user.getUsername();
-        this.password = user.getPassword();
-        this.active = user.isActive();
-        this.authorities = Arrays.stream(user.getRoles().split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+        this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
         return authorities;
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return user.getUsername();
     }
 
     @Override
@@ -57,6 +53,6 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return active;
+        return user.isEnabled();
     }
 }
